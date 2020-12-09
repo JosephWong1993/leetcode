fc0c79a3f526ffed4dc5776ed9044d8123557f3f
JosephWong <joseph_wong1993@foxmail.com>
2020-12-09 10:22:11 +0800

    Study LeetCode 328. 奇偶链表

---
+++ src/main/java/com/wong/Example/Example_00328_OddEvenLinkedList.java
@@ -0,0 +1,30 @@
+package com.wong.Example;
+
+import com.wong.Utils.ListNode;
+import org.springframework.stereotype.Component;
+
+/**
+ * 328. 奇偶链表
+ */
+@Component
+public class Example_00328_OddEvenLinkedList {
+    /**
+     * 方法一：分离节点后合并
+     */
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode evenHead = head.next;
+        ListNode odd = head;
+        ListNode even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
