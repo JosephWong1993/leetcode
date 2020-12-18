d452f51a7836cd84dc0ebe1c629f45d8eedca4e1
JosephWong <joseph_wong1993@foxmail.com>
2020-12-18 10:48:13 +0800

    Study LeetCode 17. 电话号码的字母组合

---
+++ src/main/java/com/wong/Example/Example_00017_LetterCombinationsOfPhoneNumber.java
@@ -0,0 +1,53 @@
+package com.wong.Example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 17. 电话号码的字母组合
+ * https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/
+ */
+public class Example_00017_LetterCombinationsOfPhoneNumber {
+    /**
+     * 方法一：回溯
+     */
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+        return combinations;
+    }
+    
+    private void backtrack(List<String> combinations,
+                           Map<Character, String> phoneMap,
+                           String digits,
+                           int index,
+                           StringBuffer combination) {
+        if (index < digits.length()) {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        } else {
+            combinations.add(combinations.toString());
+        }
+    }
+}
