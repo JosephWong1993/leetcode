e2ac53afcac43e12e84ceb1de0af35cebe2c4973
JosephWong <joseph_wong1993@foxmail.com>
2020-12-29 16:04:34 +0800

    Study Leetcode 347. 前 K 个高频元素

---
+++ src/main/java/com/wong/example/Example_00347_TopFrequentElements.java
@@ -0,0 +1,94 @@
+package com.wong.example;
+
+import java.util.*;
+
+/**
+ * 347. 前 K 个高频元素
+ * https://leetcode-cn.com/problems/top-k-frequent-elements/
+ */
+public class Example_00347_TopFrequentElements {
+    /**
+     * 方法一：堆
+     */
+    public int[] topKFrequent_1(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey();
+            int count = entry.getValue();
+            if (queue.size() < k) {
+                queue.offer(new int[]{num, count});
+            } else {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            }
+        }
+
+        int[] returnAry = new int[k];
+        for (int i = 0; i < k; i++) {
+            returnAry[i] = queue.poll()[0];
+        }
+
+        return returnAry;
+    }
+
+    /**
+     * 方法二：基于快速排序
+     */
+    public int[] topKFrequent_2(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        List<int[]> values = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            values.add(new int[]{num, count});
+        }
+
+        int[] ret = new int[k];
+        qsort(values, 0, values.size() - 1, ret, 0, k);
+        return ret;
+    }
+
+    private void qsort(List<int[]> values, int start, int end, int[] ret, int retIndex, int k) {
+        int picked = (int) (Math.random() * (end - start + 1)) + start;
+        Collections.swap(values, picked, start);
+
+        int pivot = values.get(start)[1];
+        int index = start;
+        for (int i = start + 1; i <= end; i++) {
+            if (values.get(i)[1] >= pivot) {
+                Collections.swap(values, index + 1, i);
+                index++;
+            }
+        }
+        Collections.swap(values, start, index);
+
+        if (k <= index - start) {
+            qsort(values, start, index - 1, ret, retIndex, k);
+        } else {
+            for (int i = start; i <= index; i++) {
+                ret[retIndex++] = values.get(i)[0];
+            }
+
+            if (k > index - start + 1) {
+                qsort(values, index + 1, end, ret, retIndex, k - (index - start + 1));
+            }
+        }
+    }
+}
