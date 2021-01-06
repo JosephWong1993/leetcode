a6e8a8c05c12b19e4b0768e3bb55f841b11abb4a
JosephWong <joseph_wong1993@foxmail.com>
2021-01-06 21:27:06 +0800

    Study 力扣 56. 合并区间

---
+++ src/main/java/com/wong/example/example_00001_00100/Example_00056_MergeIntervals.java
@@ -0,0 +1,36 @@
+package com.wong.example.example_00001_00100;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 56. 合并区间
+ * https://leetcode-cn.com/problems/merge-intervals/
+ */
+public class Example_00056_MergeIntervals {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        List<int[]> merged = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            int L = intervals[i][0], R = intervals[i][1];
+            if (merged.size() == 0 || L > merged.get(merged.size() - 1)[1]) {
+                merged.add(new int[]{L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
