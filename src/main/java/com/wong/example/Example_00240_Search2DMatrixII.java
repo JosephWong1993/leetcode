583db2f8956ce1671b34a96bf81106be31a8eaa7
JosephWong <joseph_wong1993@foxmail.com>
2021-01-09 17:33:22 +0800

    Study 力扣 240. 搜索二维矩阵 II

--- src/main/java/com/wong/example/Example_00240_Search2DMatrixII.java
+++ src/main/java/com/wong/example/Example_00240_Search2DMatrixII.java
@@ -64,5 +64,60 @@ public class Example_00240_Search2DMatrixII {
         return false;
     }
 
-    //TODO:还有其他解法
+    /**
+     * 方法三：搜索空间的缩减
+     */
+    public boolean searchMatrix_3(int[][] matrix, int target) {
+        // 一个空矩阵中显然不会包含 target
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+
+        return searchRec(matrix, target, 0, matrix.length - 1, 0, matrix[0].length - 1);
+    }
+
+    private boolean searchRec(int[][] matrix, int target, int up, int down, int left, int right) {
+        // 这个子矩阵不存在 高度 或 宽度
+        if (up > down || left > right) {
+            return false;
+        }
+        // 在这个子矩阵中 target 已经比最小的元素更小 或比 最大的元素更大
+        else if (target < matrix[up][left] || target > matrix[down][right]) {
+            return false;
+        }
+
+        int mid = (left + right) / 2;
+
+        // 锁定满足 matrix[row-1][mid] < target <matrix[row][mid] 的行
+        int row = up;
+        while (row <= down && matrix[row][mid] <= target) {
+            if (matrix[row][mid] == target) {
+                return true;
+            }
+            row++;
+        }
+
+        // 搜索 左下 和 右上 两个子矩阵
+        return searchRec(matrix, target, row, down, left, mid - 1) ||
+                searchRec(matrix, target, up, row - 1, mid + 1, right);
+    }
+
+
+    /**
+     * 方法四
+     */
+    public boolean searchMatrix_4(int[][] matrix, int target) {
+        int row = matrix.length - 1;
+        int col = 0;
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] < target) {
+                col++;
+            } else if (matrix[row][col] > target) {
+                row--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
