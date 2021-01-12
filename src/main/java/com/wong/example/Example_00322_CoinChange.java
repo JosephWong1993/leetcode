ae23f298ee2420808ef9d4094c1cba8f857c0782
JosephWong <joseph_wong1993@foxmail.com>
2021-01-12 20:49:58 +0800

    Study 力扣 322. 零钱兑换

---
+++ src/main/java/com/wong/example/Example_00322_CoinChange.java
@@ -0,0 +1,84 @@
+package com.wong.example;
+
+import java.util.Arrays;
+
+/**
+ * 322. 零钱兑换
+ * https://leetcode-cn.com/problems/coin-change/
+ */
+public class Example_00322_CoinChange {
+    /**
+     * 方法一：搜索回溯
+     */
+    public int coinChange_1(int[] coins, int amount) {
+        return coinChange(0, coins, amount);
+    }
+
+    private int coinChange(int idxCoin, int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        int minCost = Integer.MAX_VALUE;
+        if (idxCoin < coins.length && amount > 0) {
+            int maxVal = amount / coins[idxCoin];
+            for (int x = 0; x <= maxVal; x++) {
+                if (amount >= x * coins[idxCoin]) {
+                    int res = coinChange(idxCoin + 1, coins, amount - x * coins[idxCoin]);
+                    if (res != -1) {
+                        minCost = Math.min(minCost, res + x);
+                    }
+                }
+            }
+        }
+        return minCost == Integer.MAX_VALUE ? -1 : minCost;
+    }
+
+    /**
+     * 方法二：动态规划-自上而下
+     */
+    public int coinChange_2(int[] coins, int amount) {
+        if (amount < 1) {
+            return 0;
+        }
+        return coinChange(coins, amount, new int[amount]);
+    }
+
+    private int coinChange(int[] coins, int amount, int[] dp) {
+        if (amount < 0) {
+            return -1;
+        }
+        if (amount == 0) {
+            return 0;
+        }
+        if (dp[amount - 1] != 0) {
+            return dp[amount - 1];
+        }
+        int minCost = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = coinChange(coins, amount - coin, dp);
+            if (res >= 0 && res < minCost) {
+                minCost = res + 1;
+            }
+        }
+        dp[amount - 1] = minCost == Integer.MAX_VALUE ? -1 : minCost;
+        return dp[amount - 1];
+    }
+
+    /**
+     * 方法三：动态规划：自下而上
+     */
+    public int coinChange_3(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
