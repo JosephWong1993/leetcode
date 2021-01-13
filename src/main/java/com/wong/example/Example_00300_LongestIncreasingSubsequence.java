4d34a09f3bbb1f9160e078e612b65705171efb3e
JosephWong <joseph_wong1993@foxmail.com>
2021-01-13 20:37:39 +0800

    Study 力扣 300. 最长递增子序列

--- src/main/java/com/wong/example/Example_00300_LongestIncreasingSubsequence.java
+++ src/main/java/com/wong/example/Example_00300_LongestIncreasingSubsequence.java
@@ -28,5 +28,37 @@ public class Example_00300_LongestIncreasingSubsequence {
         return maxAns;
     }
 
-    //TODO：还有其他解法
+    /**
+     * 方法二：贪心 + 二分查找
+     */
+    public int lengthOfLTS_2(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int len = 1;
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > d[len]) {
+                d[++len] = nums[i];
+            } else {
+                int l = 1, r = len;
+                // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                int pos = 0;
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        //将该索引后一位更新
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
+    }
 }
