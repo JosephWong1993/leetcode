ea603697faab8af91fd9d1079e688b85d9f6d5c0
JosephWong <joseph_wong1993@foxmail.com>
2021-01-30 21:01:00 +0800

    Study 力扣 171. Excel表列序号

---
+++ src/main/java/com/wong/example/Example_00171_ExcelColumnNumber.java
@@ -0,0 +1,16 @@
+package com.wong.example;
+
+/**
+ * 171. Excel表列序号
+ * https://leetcode-cn.com/problems/excel-sheet-column-number/
+ */
+public class Example_00171_ExcelColumnNumber {
+    public int titleToNumber(String s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int num = s.charAt(i) - 'A' + 1;
+            ans = ans * 26 + num;
+        }
+        return ans;
+    }
+}
