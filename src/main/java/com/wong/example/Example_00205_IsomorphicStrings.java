df0b0fe657f08b0d0f6f9952fe9cf1c59c9d0afc
JosephWong <joseph_wong1993@foxmail.com>
2021-02-01 23:59:38 +0800

    Study 力扣 205. 同构字符串

---
+++ src/main/java/com/wong/example/Example_00205_IsomorphicStrings.java
@@ -0,0 +1,31 @@
+package com.wong.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 205. 同构字符串
+ * https://leetcode-cn.com/problems/isomorphic-strings/
+ */
+public class Example_00205_IsomorphicStrings {
+    /**
+     * 方法一：哈希表
+     */
+    public boolean isIsomorphic(String s, String t) {
+        if (s != t) {
+            Map<Character, Character> s2t = new HashMap<>();
+            Map<Character, Character> t2s = new HashMap<>();
+            for (int i = 0; i < s.length(); i++) {
+                char x = s.charAt(i);
+                char y = t.charAt(i);
+                if ((s2t.containsKey(s.charAt(i)) && s2t.get(x) != y) ||
+                        (t2s.containsKey(t.charAt(i)) && t2s.get(y) != x)) {
+                    return false;
+                }
+                s2t.put(x, y);
+                t2s.put(y, x);
+            }
+        }
+        return true;
+    }
+}
