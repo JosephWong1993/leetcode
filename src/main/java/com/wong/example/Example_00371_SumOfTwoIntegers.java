d00fb125695941fb5997e8a421b91dcf50dc70d5
JosephWong <joseph_wong1993@foxmail.com>
2021-02-07 11:40:51 +0800

    Study 力扣 371. 两整数之和

---
+++ src/main/java/com/wong/example/Example_00371_SumOfTwoIntegers.java
@@ -0,0 +1,15 @@
+package com.wong.example;
+
+public class Example_00371_SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            // 计算二进制无进位累加值
+            int temp = a ^ b;
+            // 计算二进制累加进位值
+            b = (a & b) << 1;
+            
+            a = temp;
+        }
+        return a;
+    }
+}
