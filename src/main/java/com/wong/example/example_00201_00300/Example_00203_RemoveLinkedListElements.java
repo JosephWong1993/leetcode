4d8622cd8a1c79d971a2d80619f6a50752796e90
JosephWong <joseph_wong1993@foxmail.com>
2021-03-05 10:57:03 +0800

    study 力扣 203. 移除链表元素

--- src/main/java/com/wong/example/example_00201_00300/Example_00203_RemoveLinkedListElements.java
+++ src/main/java/com/wong/example/example_00201_00300/Example_00203_RemoveLinkedListElements.java
@@ -1,4 +1,57 @@
 package com.wong.example.example_00201_00300;
 
+/**
+ * 203. 移除链表元素
+ * https://leetcode-cn.com/problems/remove-linked-list-elements/
+ */
 public class Example_00203_RemoveLinkedListElements {
+    
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+    
+    /**
+     * 方法：哨兵节点
+     */
+    public ListNode removeElements_1(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0, head);
+        ListNode prev = sentinel;
+        ListNode curr = head;
+        while (curr != null) {
+            if (curr.val == val) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return sentinel.next;
+    }
+}
+
+class ListNode {
+    int val;
+    
+    ListNode next;
+    
+    ListNode() {
+    }
+    
+    ListNode(int val) {
+        this.val = val;
+    }
+    
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
