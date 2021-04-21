1fad85c0e609d4f91ae6c5e65ff06fc1148b2f29
Joseph Wong <joseph_wong1993@foxmail.com>
2021-04-21 11:11:41 +0800

    study 力扣 292. Nim 游戏

---
+++ src/main/java/com/wong/example/example_00201_00300/Example_00292_NimGame.java
@@ -0,0 +1,11 @@
+package com.wong.example.example_00201_00300;
+
+/**
+ * 292. Nim 游戏
+ * https://leetcode-cn.com/problems/nim-game/
+ */
+public class Example_00292_NimGame {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
